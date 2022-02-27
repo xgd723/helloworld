3a0e6d0609db291e0e49a351061c6a51115228a9
xgd723 <62703782+xgd723@users.noreply.github.com>
2022-02-28 01:16:33 +0800

    Update test2.java

--- src/test2.java
+++ src/test2.java
@@ -2,6 +2,6 @@ import java.sql.SQLOutput;
 
 public class test2 {
     public static void main(String[] args) {
-        System.out.println("hahahahha");
+        System.out.println("ha");
     }
 }
