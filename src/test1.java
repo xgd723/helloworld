988e99da6d204879ac6bfc1c4534584d3adf10d0
xgd <2319937064@qq.com>
2022-02-28 00:17:10 +0800

    test2 test

--- src/test1.java
+++ src/test1.java
@@ -2,5 +2,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("test1");
+        System.out.println("test2");
     }
 }
