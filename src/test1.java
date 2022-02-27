540984060c8e5b87b3aeef822d4ed00891891a73
xgd723 <62703782+xgd723@users.noreply.github.com>
2022-02-28 01:20:02 +0800

    Update test1.java

--- src/test1.java
+++ src/test1.java
@@ -1,8 +1,10 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("test1");
+        System.out.println("test11111");
         System.out.println("test2");
         System.out.println("test33");
+        System.out.println("test33444");
+
     }
 }
